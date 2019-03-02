095ec9688e3dd80c85e1168d274cc6d4bc1b3c6c
Aaron Shaver <AaronShaverPDX@gmail.com>
2019-03-02 11:11:18 -0800

    Passes challenge #2 more than half the time

--- main.js
+++ main.js
@@ -1,38 +1,36 @@
 {
     init: function(elevators, floors) {
-        var elevator = elevators[0]; // Let's use the first elevator
+        const topFloor = floors.length - 1;
+        var elevator = elevators[0];
 
-        // Whenever the elevator is idle (has no more queued destinations) ...
         elevator.on("idle", function() {
 
-            // Listen for up and down button presses, and queue those right away
-            floors.forEach(function(floor) {
-                floor.on("up_button_pressed", function() {
-                    if(elevator.loadFactor() < 0.25) {                
-                        // go to floor of pressed up indicator right away
-                        elevator.goToFloor(floor.floorNum(), true);
-                    }
+            var pressedFloors = elevator.getPressedFloors();
+            if(pressedFloors.length == 0) {
+                // do the default, dumb moving behavior when there are no passengers
+                defaultMovement();
+            }
+            else {
+                // there are pressed floors (passengers in elevator), so move to first pressed floor in the array
+                pressedFloors.every(function(floorNum) {
+                    elevator.goToFloor(floorNum);
+                    return false;
                 });
+            }
 
-                floor.on("down_button_pressed", function() {
-                    if(elevator.loadFactor() < 0.25) {                
-                        // go to floor of pressed up indicator right away
-                        elevator.goToFloor(floor.floorNum(), true);
-                    }
-                });
-            });
-
-            elevator.goToFloor(0);
-            elevator.goToFloor(1);
-            elevator.goToFloor(2);
-            elevator.goToFloor(3);
-            elevator.goToFloor(4);
-            elevator.goToFloor(3);
-            elevator.goToFloor(2);
-            elevator.goToFloor(1);
+            function defaultMovement() {
+                // if current floor isn't top floor go up; otherwise, go to ground floor
+                var currentFloor = elevator.currentFloor()
+                if(currentFloor < topFloor) {
+                    elevator.goToFloor(currentFloor + 1)
+                }
+                else {
+                    elevator.goToFloor(0)
+                }
+            }
         });
     },
-    update: function(dt, elevators, floors) {
-        // We normally don't need to do anything here
-    }
+        update: function(dt, elevators, floors) {
+            // We normally don't need to do anything here
+        }
 }
