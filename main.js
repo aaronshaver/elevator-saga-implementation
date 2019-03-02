ab067616ef0d9c42dbc9a0233667518f3a4295d2
Aaron Shaver <AaronShaverPDX@gmail.com>
2019-03-02 12:28:52 -0800

    Print elevator status; clear console at start

--- main.js
+++ main.js
@@ -1,13 +1,14 @@
 {
     init: function(elevators, floors) {
+        console.clear();
         const topFloor = floors.length - 1;
         var elevator = elevators[0];
 
         elevator.on("idle", function() {
-
             var pressedFloors = elevator.getPressedFloors();
+
             if(pressedFloors.length == 0) {
-                // do the default, dumb moving behavior when there are no passengers
+                // do the default, dumb moving behavior when there are no passengers and no up button presses
                 defaultMovement();
             }
             else {
@@ -18,19 +19,27 @@
                 });
             }
 
-            function defaultMovement() {
-                // if current floor isn't top floor go up; otherwise, go to ground floor
-                var currentFloor = elevator.currentFloor()
-                if(currentFloor < topFloor) {
-                    elevator.goToFloor(currentFloor + 1)
-                }
-                else {
-                    elevator.goToFloor(0)
-                }
-            }
         });
+
+        function defaultMovement() {
+            // if current floor isn't top floor go up; otherwise, go to ground floor
+            var currentFloor = elevator.currentFloor()
+            if(currentFloor < topFloor) {
+                elevator.goToFloor(currentFloor + 1)
+            }
+            else {
+                elevator.goToFloor(0)
+            }
+        }
+
     },
         update: function(dt, elevators, floors) {
-            // We normally don't need to do anything here
+            elevator = elevators[0];
+            console.log('----------------------------------------------------------');
+            var output = '';
+            output += 'pressed floors: ' + elevator.getPressedFloors().length + ' | ';
+            output += 'current floor: ' + elevator.currentFloor() + ' | ';
+            output += 'load: ' + elevator.loadFactor() + ' | ';
+            console.log(output);
         }
 }
