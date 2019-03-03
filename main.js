bf70ab1bc7e3e5793e6fe3737bb3f3cec046369e
Aaron Shaver <AaronShaverPDX@gmail.com>
2019-03-02 17:52:49 -0800

    Now goes to most popular origin of up/down pushes

--- main.js
+++ main.js
@@ -1,22 +1,45 @@
 {
     init: function(elevators, floors) {
         console.clear();
+        var originsOfRequests = [];
         const topFloor = floors.length - 1;
         var elevator = elevators[0];
+
+        floors.forEach(function(floor) {
+            var floorNum = floor.floorNum();
+            console.log("adding listeners for floor ", floorNum)
+
+            floor.on("up_button_pressed", function() {
+                originsOfRequests.push(floorNum);
+                printOriginsOfRequests();
+            });
+
+            floor.on("down_button_pressed", function() {
+                originsOfRequests.push(floorNum);
+                printOriginsOfRequests();
+            });
+
+        });
 
         elevator.on("idle", function() {
             var pressedFloors = elevator.getPressedFloors();
 
             if(pressedFloors.length == 0) {
-                // do the default, dumb moving behavior when there are no passengers and no up button presses
+                if(originsOfRequests.length > 0) {
+                    // get most popular floor among the floors that have pressed up/down buttons
+                    var floorNum = mode(originsOfRequests);
+                    elevator.goToFloor(floorNum);
+                    // remove that floor from the array of origins of up/down presses
+                    var filteredArray = originsOfRequests.filter(e => e !== floorNum);
+                    originsOfRequests = filteredArray;
+                }
+
+                // do the default, dumb moving behavior when there are no passengers and no requests from other floors
                 defaultMovement();
             }
             else {
                 // there are pressed floors (passengers in elevator), so move to first pressed floor in the array
-                pressedFloors.every(function(floorNum) {
-                    elevator.goToFloor(floorNum);
-                    return false;
-                });
+                elevator.goToFloor(pressedFloors[0]);
             }
 
         });
@@ -32,6 +55,25 @@
             }
         }
 
+        function printOriginsOfRequests() {
+            console.log('origin of request floors: ' + originsOfRequests); 
+        }
+
+        var mode = function mode(arr) {
+            // borrowed from https://codereview.stackexchange.com/questions/68315/finding-the-mode-of-an-array
+            var numMapping = {};
+            var greatestFreq = 0;
+            var mode;
+            arr.forEach(function findMode(number) {
+                numMapping[number] = (numMapping[number] || 0) + 1;
+        
+                if (greatestFreq < numMapping[number]) {
+                    greatestFreq = numMapping[number];
+                    mode = number;
+                }
+            });
+            return +mode;
+        }
     },
         update: function(dt, elevators, floors) {
             elevator = elevators[0];
